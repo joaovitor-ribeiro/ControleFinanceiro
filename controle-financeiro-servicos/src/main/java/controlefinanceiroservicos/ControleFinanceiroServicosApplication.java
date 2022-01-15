fcf44b8954712fef48a221d4e5d30476b7e641cc
joaovitor-ribeiro <joaosvit10@hotmail.com>
2022-01-15 14:19:05 -0300

    Projeto criado

---
+++ controle-financeiro-servicos/src/main/java/controlefinanceiroservicos/ControleFinanceiroServicosApplication.java
@@ -0,0 +1,13 @@
+package controlefinanceiroservicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleFinanceiroServicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleFinanceiroServicosApplication.class, args);
+	}
+
+}
