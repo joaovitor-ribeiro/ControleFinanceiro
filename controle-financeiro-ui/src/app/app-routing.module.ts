613d848f7887a75b6dbf4c4d30f3eb7af049aafd
joaovitor-ribeiro <joaosvit10@hotmail.com>
2022-08-29 20:53:26 -0300

    Login

--- controle-financeiro-ui/src/app/app-routing.module.ts
+++ controle-financeiro-ui/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PainelComponent } from './painel/painel.component';
 
-
 const routes: Routes = [
   {
     path: 'despesa',
@@ -30,7 +29,7 @@ const routes: Routes = [
   },
 
   {path: 'painel', component: PainelComponent},
-  
+
   {
     path: 'entrar',
     loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
